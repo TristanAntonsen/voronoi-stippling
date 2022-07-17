350a3842053fb53ea18cbf2e6bc5c109a7f10cdf
TristanAntonsenFR <84728289+TristanAntonsenFR@users.noreply.github.com>
2022-07-17 12:37:55 -0500

    moving functions into other file

--- rasterize.py
+++ rasterize.py
@@ -64,8 +64,18 @@ def Bounding_Box(polygon):
 
     return ([x_min, x_max],[y_min,y_max])
 
+def Calculate_Angle(u,v):
+
+    numerator = np.dot(u,v)
+    denominator = np.linalg.norm(u) * np.linalg.norm(v)
+
+    theta = np.arccos(numerator / denominator)
+
+    return theta
+
+##### Brute force rasterization #####
 def Rasterize_Polygon(polygon):
-    
+    op_count = 0
     bbox = Bounding_Box(polygon)
     points = []
     for x in range(*bbox[0]):
@@ -80,6 +90,7 @@ def Rasterize_Polygon(polygon):
                         v1 = 0
                     edge_state = Edge_Function(polygon[v0],polygon[v1],[x,y])
                     state = state and edge_state
+                    op_count += 1
                 
                 ## If on positive side of all lines
                 if state:
@@ -87,11 +98,105 @@ def Rasterize_Polygon(polygon):
                     
     return points
 
-def Calculate_Angle(u,v):
+##### Scanline rasterization #####
 
-    numerator = np.dot(u,v)
-    denominator = np.linalg.norm(u) * np.linalg.norm(v)
+def Line_Intersection(p1, p2, p3, p4):
 
-    theta = np.arccos(numerator / denominator)
+    ## Line 1
+    x1 = p1[0]
+    y1 = p1[1]
+    x2 = p2[0]
+    y2 = p2[1]
 
-    return theta
+    ## Line 2
+    x3 = p3[0]
+    y3 = p3[1]
+    x4 = p4[0]
+    y4 = p4[1]
+
+    ## p_x
+
+    p_x_num = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)
+    p_x_denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+    p_y_num = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)
+    p_y_denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+    p_x = p_x_num / p_x_denom
+    p_y = p_y_num / p_y_denom
+
+    return (p_x, p_y)
+
+
+
+def Edges(polygon):
+    edges = []
+    for i, point in enumerate(polygon):
+
+        k = i + 1
+        if k == len(polygon):
+            k = 0
+        point2 = polygon[k]
+
+        edge = [point, point2]
+
+        edges.append(edge)
+    
+    return edges
+
+
+def Scanline_nodes(polygon,scan_y, image_res):
+
+    polygon_edges = Edges(polygon)
+    nodes = []
+    for edge in polygon_edges:
+        edge = list(edge)
+        p1y = edge[0][1]
+        p2y = edge[1][1]
+
+        if p1y < scan_y and p2y >= scan_y or p1y >= scan_y and p2y < scan_y:
+            p1x = edge[0][0]
+            p2x = edge[1][0]
+
+            p1 = [p1x,p1y]
+            p2 = [p2x,p2y]
+            p3 = [0,scan_y]
+            p4 = [image_res,scan_y]
+            node = Line_Intersection(p1,p2,p3,p4)
+
+            nodes.append(node)
+
+    if len(nodes) < 1:
+        return False
+    else:
+        return nodes
+
+
+def Raster_Scanline(nodes, scan_y, raster_res, image_res):
+    scale_factor = image_res / raster_res
+    x1 = min(nodes[0][0],round(nodes[1][0]))
+    x2 = max(nodes[0][0],round(nodes[1][0]))
+
+    raster_y = round(scan_y / scale_factor) * scale_factor
+    raster_x = round(x1 / scale_factor) * scale_factor
+
+    pixels = []
+
+    while raster_x < x2:
+        pixels.append([raster_x,raster_y])
+        raster_x += scale_factor
+
+    return pixels
+
+def Rasterize_Polygon(polygon, bbox, raster_res, image_res):
+
+    scale_factor = image_res / raster_res
+    scan_y = bbox[0][1]
+    pixels = []
+    while scan_y < bbox[1][1]:
+        nodes = Scanline_nodes(polygon,scan_y, image_res)
+        if nodes:
+            pixels += Raster_Scanline(nodes, scan_y, raster_res, image_res)
+        scan_y += scale_factor
+
+    return pixels, scale_factor
--- scanline_rasterization.py
+++ scanline_rasterization.py
@@ -1,7 +1,7 @@
 from turtle import width
 import numpy as np
 from PIL import Image, ImageDraw
-from rasterize import Rasterize_Polygon, Sort_Vertices, Calculate_Centroid
+from rasterize import Rasterize_Polygon, Sort_Vertices, Calculate_Centroid, Edges, Raster_Scanline, Scanline_nodes
 import time
 
 t0 = time.time()
@@ -41,125 +41,12 @@ polygon = [p * scale_factor_init for p in polygon]
 sorted_polygon = Sort_Vertices(polygon)
 
 
-
-# for point in rasterized:
-#     x = point[0] * scale_factor
-#     y = point[1] * scale_factor
-
-    # center_rectangle((x + 0.5), (y + 0.5), scale_factor / 2, scale_factor / 2, 'pink')
-    # center_ellipse((x + 0.5), (y + 0.5), scale_factor / 2, 'pink')
-    
-# Outline_Poly(sorted_polygon,'rgb(255,255,255)', 3)
-
-# for p in sorted_polygon:
-#     center_ellipse(p[0],p[1],10,'white')
-
-
-
-def Line_Intersection(p1, p2, p3, p4):
-
-    ## Line 1
-    x1 = p1[0]
-    y1 = p1[1]
-    x2 = p2[0]
-    y2 = p2[1]
-
-    ## Line 2
-    x3 = p3[0]
-    y3 = p3[1]
-    x4 = p4[0]
-    y4 = p4[1]
-
-    ## p_x
-
-    p_x_num = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)
-    p_x_denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-
-    p_y_num = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)
-    p_y_denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-
-    p_x = p_x_num / p_x_denom
-    p_y = p_y_num / p_y_denom
-
-    return (p_x, p_y)
-
-
-
-def Edges(polygon):
-    edges = []
-    for i, point in enumerate(polygon):
-
-        k = i + 1
-        if k == len(polygon):
-            k = 0
-        point2 = polygon[k]
-
-        edge = [point, point2]
-
-        edges.append(edge)
-    
-    return edges
-
-
-def Scanline_nodes(polygon,scan_y):
-
-    polygon_edges = Edges(polygon)
-    nodes = []
-    for edge in polygon_edges:
-        edge = list(edge)
-        p1y = edge[0][1]
-        p2y = edge[1][1]
-
-        if p1y < scan_y and p2y >= scan_y or p1y >= scan_y and p2y < scan_y:
-            p1x = edge[0][0]
-            p2x = edge[1][0]
-
-            p1 = [p1x,p1y]
-            p2 = [p2x,p2y]
-            p3 = [0,scan_y]
-            p4 = [image_resolution,scan_y]
-            node = Line_Intersection(p1,p2,p3,p4)
-
-            nodes.append(node)
-
-    if len(nodes) < 1:
-        return False
-    else:
-        return nodes
-
 def Visualize_Scanline(nodes, scan_y):
     x1 = min(round(nodes[0][0]),round(nodes[1][0]))
     x2 = max(round(nodes[0][0]),round(nodes[1][0]))
     x = x1
     draw.line((x1,scan_y,x2,scan_y),fill='white',width=10)
 
-
-def Raster_Scanline(nodes, scan_y, raster_res, image_res):
-    scale_factor = image_res / raster_res
-    x1 = min(nodes[0][0],round(nodes[1][0]))
-    x2 = max(nodes[0][0],round(nodes[1][0]))
-
-    raster_y = round(scan_y / scale_factor) * scale_factor
-    raster_x = round(x1 / scale_factor) * scale_factor
-
-    pixels = []
-
-    while raster_x < x2:
-        pixels.append([raster_x,raster_y])
-        raster_x += scale_factor
-    return pixels
-
-def Rasterize_Polygon(polygon, bbox, raster_res, image_res):
-
-    scale_factor = image_res / raster_res
-    scan_y = bbox[0][1]
-    pixels = []
-    while scan_y < bbox[1][1]:
-        nodes = Scanline_nodes(polygon,scan_y)
-        if nodes:
-            pixels += Raster_Scanline(nodes, scan_y, raster_res, image_resolution)
-        scan_y += scale_factor
-    return pixels, scale_factor
 
 bbox = [[0,0],[image_resolution,image_resolution]]
 raster_res = 100
