fde753225430f521b8642a4ee8515cec81753e22
TristanAntonsenFR <84728289+TristanAntonsenFR@users.noreply.github.com>
2022-08-06 17:37:19 -0500

    initial rust setup (rust files)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -8,9 +7,72 @@ fn demo_function(a: usize, b: usize) -> PyResult<usize> {
     Ok(result)
 }
 
+/// Calculates weighted centroid of pixels.
+/// pixels -> list (nested) [[x,y],[x,y]...]
+/// weights -> list (nested) [resolution, resolution]
+#[pyfunction]
+fn weighted_raster_centroid(pixels: Vec<[f32; 2]>, weights: Vec<[f32; 1080]>) -> PyResult<(f32, f32)> {
+
+    let pixel_count = pixels.len();
+    // let increment_size = round::ceil(pixel_count);
+    // let increment_size = (pixel_count as f32 / 500.0).ceil();
+    
+    let mut pixel;
+    let mut cx = 0.0;
+    let mut cy = 0.0;
+    let mut sample_x;
+    let mut sample_y;
+    let mut sampled_value;
+    let mut weight;
+    let mut total_weight = 0.0;
+
+
+    let mut i = 0;
+    while i < pixel_count {
+        pixel = &pixels[i];
+
+        sample_x = pixel[1] as usize;
+        sample_y = pixel[0] as usize;
+        sampled_value = weights[sample_x][sample_y];
+
+        weight = 1.0 - sampled_value;
+        total_weight += weight;
+
+        cx += pixel[0] * weight;
+        cy += pixel[1] * weight;
+
+        i += 1;
+    }
+
+    if total_weight == 0.0 {
+        total_weight = pixel_count as f32;
+    }
+
+    cx /= total_weight;
+    cy /= total_weight;
+
+    let result;
+
+    if pixel_count == 0 {
+        result = (0.0,0.0);
+    } else if cx == 0.0 {
+        result = (0.0,0.0);
+    } else if cy == 0.0 {
+        result = (0.0,0.0);
+    } else {
+        result = (cx, cy);
+    }
+
+    Ok(result)
+
+}
+
+
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn voronoi_stippling(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(demo_function, m)?)?;
+    m.add_function(wrap_pyfunction!(weighted_raster_centroid, m)?)?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello World!");
+    let test = vec![[1,2],[1,2]];
+
+    print_type_of(&test);
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
