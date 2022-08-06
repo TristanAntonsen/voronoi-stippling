dadd6313f904a774db3f17d48afdbb8f740c21a8
TristanAntonsenFR <84728289+TristanAntonsenFR@users.noreply.github.com>
2022-08-06 14:08:06 -0500

    updating readme & demo function

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# voronoi-stippling
-Weighted voronoi point stippling
+# Weighted voronoi point stippling
 
-Activating virtual environment:
+## Activating virtual environment:
 
 (Windows)
 
@@ -18,3 +17,13 @@ Compiling rust library:
 or
 
     maturin develop --release
+
+## Running rust tests
+
+    cargo run --bin tests
+
+## Python usage of Rust library
+    >>> import voronoi_stippling as vs
+    >>>
+    >>> vs.demo_function(10,10) # multiplies arguments (rust)
+    100
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,14 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn demo_function(a: usize, b: usize) -> PyResult<usize> {
+    let result = a * b;
+    Ok(result)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn voronoi_stippling(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(demo_function, m)?)?;
     Ok(())
 }
